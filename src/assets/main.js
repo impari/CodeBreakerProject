840e17fea82c1183510f5d613c0346a763c02a51
Parimal Chandwaskar <parimal.chandwaskar@gmail.com>
2017-10-07 16:43:33 +0530

    First commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,9 +1,14 @@
-let answer = document.getElementById('answer');
-let attempt = document.getElementById('attempt');
+var answer = document.getElementById('answer');
+var attempt = document.getElementById('attempt');
 
 function guess() {
-    let input = document.getElementById('user-guess');
+    var input = document.getElementById('user-guess');
     //add functionality to guess function here
 }
 
 //implement new functions here
+
+function setHiddenFields(){
+	var random = Math.random();
+	answer = Math.floor(random * 9999);
+}
