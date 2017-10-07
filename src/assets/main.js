2572de568b5f0a4d2dd62b0ead09afb054bd638a
Parimal Chandwaskar <parimal.chandwaskar@gmail.com>
2017-10-07 19:08:21 +0530

    Half way

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,6 +4,14 @@ var attempt = document.getElementById('attempt');
 function guess() {
     var input = document.getElementById('user-guess');
     //add functionality to guess function here
+    debugger
+    if(answer.value == '' || attempt.value == ''){
+    	setHiddenFields();
+    }
+    if(input.value){
+    	validateInput(input.value);
+    	attempt.value ++;
+    }
 }
 
 //implement new functions here
@@ -13,6 +21,19 @@ function setHiddenFields(){
 	while(answer.value.length<4){
 		answer.value = "0"+ answer.value;
 	}
-    
+    attempt.value = "0";
+}
 
+function setMessage(message){
+	document.getElementById('message').innerHTML = message;
+}
+
+
+function validateInput(validate){
+	if(validate.length == 4){
+		return true;
+	} else {
+		setMessage("Guesses must be exactly 4 characters long");
+		return false;
+	}
 }
